f567191e95aeebcab88652690be68a6747b6e440
moeibrahim28 <m.ibrahim0228@gmail.com>
2022-06-23 12:16:50 -0400

    Organized code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,20 @@
 public class Main {
     
     public static void main(String[] args) {
+        
         NumberContainer myNumber = new NumberContainer();
         multiplyNumbers(myNumber, 12, 3);
         System.out.println("Result = " + myNumber.number);
+
         NumberContainer myNumber2 = new NumberContainer();
-        multiplyNumbersCantSee(myNumber, 12, 3);
+        multiplyNumbersCantSee(myNumber2, 12, 3);
         System.out.println("Result = " + myNumber.number);
+
         StringContainer myString = new StringContainer();
         myString.text = "initial value";
         addText(myString, " - adding new text");
         System.out.println("modified text: " + myString.text);
+
         String myString2 = "initial value";
         addTextCantSee(myString2, " - adding new text");
         System.out.println("modified text: " + myString2);
@@ -19,13 +23,16 @@ public class Main {
     public static void multiplyNumbers(NumberContainer result, int firstNumber, int secondNumber) {
         result.number = firstNumber * secondNumber;
     }
+
     public static void multiplyNumbersCantSee(NumberContainer result, int firstNumber, int secondNumber) {
         result = new NumberContainer();
         result.number = firstNumber * secondNumber;
     }
+
     public static void addText(StringContainer result, String newText) {
         result.text = result.text + newText;
     }
+
     public static void addTextCantSee(String result, String newText) {
         result = result + newText;
     }
